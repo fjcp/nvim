99a98040974ef0614e0f01024ab1710629f5d8b6
francisco castillo <francisco.castillo@sener.es>
2021-06-23 10:24:07 +0200

    remove unused lsp servers

--- lua/language-servers/init.lua
+++ lua/language-servers/init.lua
@@ -3,17 +3,11 @@ capabilities.textDocument.completion.completionItem.snippetSupport = true
 
 -- Language servers
 
-require'lspconfig'.bashls.setup {}
-require'lspconfig'.cssls.setup {}
-require'lspconfig'.html.setup {capabilities = capabilities}
-require'lspconfig'.tsserver.setup {}
 require'lspconfig'.pyright.setup {}
 require'lspconfig'.jedi_language_server.setup {}
 require'lspconfig'.vimls.setup {}
-require'lspconfig'.yamlls.setup {}
-require'lspconfig'.solargraph.setup {}
-require'lspconfig'.vuels.setup {}
-require'lspconfig'.phpactor.setup {}
+require'lspconfig'.clangd.setup {}
+require'lspconfig'.cmake.setup {}
 require'lspconfig'.jsonls.setup {
     commands = {
         Format = {
@@ -24,41 +18,9 @@ require'lspconfig'.jsonls.setup {
     }
 }
 
-require'lspconfig'.gopls.setup {cmd = {"gopls", "serve"}, settings = {gopls = {analyses = {unusedparams = true}, staticcheck = true}}}
 
 -- Lua is a little bit different
 
-USER = vim.fn.expand('$USER')
-
-local sumneko_root_path = ""
-local sumneko_binary = ""
-
-if vim.fn.has("mac") == 1 then
-    sumneko_root_path = "/Users/" .. USER .. "/.config/nvim/ls/lua-language-server"
-    sumneko_binary = "/Users/" .. USER .. "/.config/nvim/ls/lua-language-server/bin/macOS/lua-language-server"
-elseif vim.fn.has("unix") == 1 then
-    sumneko_root_path = "/home/" .. USER .. "/.config/nvim/ls/lua-language-server"
-    sumneko_binary = "/home/" .. USER .. "/.config/nvim/ls/lua-language-server/bin/Linux/lua-language-server"
-elseif vim.fn.has("win32") == 1 then
-    sumneko_root_path = "C:\\Users" .. USER .. "\\.config/nvim\\ls\\lua-language-server"
-    sumneko_binary = "C:\\Users" .. USER .. "\\.config\\nvim\\ls\\lua-language-server\\bin\\Windows\\lua-language-server"
-else
-    print("Unsupported system for sumneko")
-end
-
-require'lspconfig'.sumneko_lua.setup {
-    cmd = {sumneko_binary, "-E", sumneko_root_path .. "/main.lua"},
-    settings = {
-        Lua = {
-            runtime = {version = 'LuaJIT', path = vim.split(package.path, ';')},
-            diagnostics = {globals = {'vim'}},
-            workspace = {
-                library = {[vim.fn.expand('$VIMRUNTIME/lua')] = true, [vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true},
-                preloadFileSize = 450
-            }
-        }
-    }
-}
 
 -- Diagnostics
 
