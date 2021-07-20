4d9b84ac3e55d0279197e88f5ec5a624bb15b29e
Francisco.Castillo <francisco.castillo@sener.es>
2021-07-20 15:46:58 +0200

    fix error in telescope preview

--- lua/telescope-config/init.lua
+++ lua/telescope-config/init.lua
@@ -1,25 +1,23 @@
 local actions = require('telescope.actions')
 require('telescope').setup {
     defaults = {
-        find_command = {'rg', '--no-heading', '--with-filename', '--line-number', '--column', '--smart-case'},
---        prompt_position = "top",
-        prompt_prefix = " ",
-        selection_caret = " ",
+        vimgrep_arguments = {
+            'rg', '--color=never', '--no-heading', '--with-filename',
+            '--line-number', '--column', '--smart-case'
+        },
+        prompt_prefix = "> ",
+        selection_caret = "> ",
         entry_prefix = "  ",
         initial_mode = "insert",
         selection_strategy = "reset",
         sorting_strategy = "descending",
         layout_strategy = "horizontal",
-        layout_config = {horizontal = {mirror = false}, vertical = {mirror = false}},
+        layout_config = {horizontal = {preview_cutoff = 100}},
         file_sorter = require'telescope.sorters'.get_fuzzy_file,
         file_ignore_patterns = {},
-        generic_sorter =require'telescope.sorters'.get_generic_fuzzy_sorter,
-        path_display = true,
+        generic_sorter = require'telescope.sorters'.get_generic_fuzzy_sorter,
+--        path_display = true,
         winblend = 0,
---        width = 0.75,
---        preview_cutoff = 120,
---        results_height = 1,
---        results_width = 0.8,
         border = {},
         borderchars = {'─', '│', '─', '│', '╭', '╮', '╯', '╰'},
         color_devicons = true,
@@ -28,20 +26,8 @@ require('telescope').setup {
         file_previewer = require'telescope.previewers'.vim_buffer_cat.new,
         grep_previewer = require'telescope.previewers'.vim_buffer_vimgrep.new,
         qflist_previewer = require'telescope.previewers'.vim_buffer_qflist.new,
-        buffer_previewer_maker = require'telescope.previewers'.buffer_previewer_maker,
-        mappings = {
-            i = {
-                ["<C-j>"] = actions.move_selection_next,
-                ["<C-k>"] = actions.move_selection_previous,
-                ["<C-q>"] = actions.smart_send_to_qflist + actions.open_qflist,
-                ["<esc>"] = actions.close,
-                ["<CR>"] = actions.select_default + actions.center
-            },
-            n = {
-                ["<C-j>"] = actions.move_selection_next,
-                ["<C-k>"] = actions.move_selection_previous,
-                ["<C-q>"] = actions.smart_send_to_qflist + actions.open_qflist,
-            }
-        }
+
+        -- Developer configurations: Not meant for general override
+        buffer_previewer_maker = require'telescope.previewers'.buffer_previewer_maker
     }
 }
