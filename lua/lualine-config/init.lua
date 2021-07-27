4738f59f6cd96c44883f2a69cc902f0e8ad31425
Francisco.Castillo <francisco.castillo@sener.es>
2021-07-27 09:46:45 +0200

    full lualine config

--- lua/lualine-config/init.lua
+++ lua/lualine-config/init.lua
@@ -1,5 +1,32 @@
+local lualine = require 'lualine'
 
-require'lualine'.setup {
+-- Color table for highlights
+local colors = {
+  bg = '#202328',
+  fg = '#bbc2cf',
+  yellow = '#ECBE7B',
+  cyan = '#008080',
+  darkblue = '#081633',
+  green = '#98be65',
+  orange = '#FF8800',
+  violet = '#a9a1e1',
+  magenta = '#c678dd',
+  blue = '#51afef',
+  red = '#ec5f67'
+}
+
+local conditions = {
+  buffer_not_empty = function() return vim.fn.empty(vim.fn.expand('%:t')) ~= 1 end,
+  hide_in_width = function() return vim.fn.winwidth(0) > 80 end,
+  check_git_workspace = function()
+    local filepath = vim.fn.expand('%:p:h')
+    local gitdir = vim.fn.finddir('.git', filepath .. ';')
+    return gitdir and #gitdir > 0 and #gitdir < #filepath
+  end
+}
+
+-- Config
+local config = {
   options = {
     icons_enabled = true,
     theme = 'gruvbox',
@@ -8,21 +35,56 @@ require'lualine'.setup {
     disabled_filetypes = {}
   },
   sections = {
+    -- these are to remove the defaults
     lualine_a = {'mode'},
     lualine_b = {'branch'},
-    lualine_c = {'filename'},
+    lualine_c = {},
     lualine_x = {'encoding', 'fileformat', 'filetype'},
     lualine_y = {'progress'},
     lualine_z = {'location'}
   },
   inactive_sections = {
+    -- these are to remove the defaults
     lualine_a = {},
-    lualine_b = {},
-    lualine_c = {'filename'},
-    lualine_x = {'location'},
+    lualine_v = {},
     lualine_y = {},
-    lualine_z = {}
-  },
-  tabline = {},
-  extensions = {}
+    lualine_z = {},
+    lualine_c = {},
+    lualine_x = {}
+  }
 }
+
+-- Inserts a component in lualine_c at left section
+local function ins_left(component)
+  table.insert(config.sections.lualine_c, component)
+end
+
+-- Inserts a component in lualine_x ot right section
+local function ins_right(component)
+  table.insert(config.sections.lualine_x, component)
+end
+
+ins_left{
+  'filename',
+  file_status = true,
+  path = 2
+  }
+
+ins_left {
+  'diagnostics',
+  sources = {'nvim_lsp'},
+  symbols = {error = ' ', warn = ' ', info = ' '},
+  color_error = colors.red,
+  color_warn = colors.yellow,
+  color_info = colors.cyan
+}
+
+ins_left{
+  'diff',
+  colored = true,
+  symbols = {added = ' ', modified = '柳 ', removed = ' '},
+  color_added = colors.green,
+  color_modified = colors.orange,
+  color_removed = colors.red,
+}
+lualine.setup(config)
